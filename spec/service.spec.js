9482cd6d8d4973b656204b9ba7b0eec7798a056f
Vlad Timofeev <timofeev040394@gmail.com>
2019-09-25 11:02:29 +0300

    Работу закончил

--- spec/service.spec.js
+++ spec/service.spec.js
@@ -8,10 +8,12 @@ describe("Server", () => {
     afterAll(() => {
 
     });
-    describe("GET /dates?months=2019-01,2019-02", () => {
+
+    //test GET
+    describe("GET /dates?months=2019-01,2019-02,2019-03", () => {
         var data = {};
         beforeAll((done) => {
-            Request.get("http://localhost:3000/dates?months=2019-01,2019-02", (error, response, body) => {
+            Request.get("http://localhost:3000/dates?months=2019-01,2019-02,2019-03", (error, response, body) => {
                 data.status = response.statusCode;
                 data.response = response.body;
                 data.body = body;
@@ -22,8 +24,22 @@ describe("Server", () => {
             expect(data.status).toBe(200);
         });
         it("Body", () => {
-            expect(data.response).toEqual('["2019-01-05","2019-01-06","2019-01-12","2019-01-13","2019-01-19","2019-01-20","2019-01-26","2019-01-27","2019-02-02","2019-02-03","2019-02-09","2019-02-10","2019-02-16","2019-02-17","2019-02-23","2019-02-24"]');
+            expect(data.response).toEqual('["2019-01-05","2019-01-06","2019-01-12","2019-01-13","2019-01-19","2019-01-20","2019-01-26","2019-01-27","2019-02-02","2019-02-03","2019-02-09","2019-02-10","2019-02-16","2019-02-17","2019-02-23","2019-02-24","2019-03-02","2019-03-03","2019-03-09","2019-03-10","2019-03-16","2019-03-17","2019-03-23","2019-03-24","2019-03-30","2019-03-31"]');
         });
     });
+
+    //test POST
+    var myJSONObject = [
+        { "date": "2019-01-01", "value": false },
+        { "date": "2019-01-03", "value": true },
+        { "date": "2019-01-04", "value": true },
+        { "date": "2019-01-05", "value": true }
+    ];
+    Request({
+        url: "http://127.0.0.1:3000/dates/save",
+        method: "POST",
+        json: true,
+        body: myJSONObject
+    }, function(error, response, body) {});
 
 });
--- testing_service.js
+++ testing_service.js
@@ -2,17 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.listen(3000, function() {
+    console.log('application has been initiated!')
+});
+
 app.use(bodyParser.json({
 
 }));
 
 let dateStorage = [];
 
-app.listen(3000, function() { //открываем порт и приложение запускает сервер; 3000 - номер порта
-    console.log('application has been initiated!')
-});
-
-//прописываем роуты. есть несколько типов запросов и им нужен роут.
+//routes for requests GET and POST
 app.get('/dates', (req, res) => {
     let m = req.query.months;
     let weekendDays = createWeekendDaysArray(m);
@@ -21,36 +21,34 @@ app.get('/dates', (req, res) => {
 });
 
 app.post('/dates/save', (req, res) => {
-    dateStorage = req.body.filter(x => x.value).map(d => d.date);
+    dateStorage = req.body.filter(x => x.value).map(y => y.date); //choose only true dates
 
     res.send(dateStorage);
 });
 
+//helping funcs
 function isWeekendDay(date) {
     let dayOfWeek = date.getDay();
-
-
 
     return dayOfWeek === 0 || dayOfWeek === 6;
 }
 
 function createWeekendDaysArray(dateStr) {
-    let dateArr = dateStr.split(',').map(x => new Date(x)); // (x) => { return new Date(x) };
+    let dateArr = dateStr.split(',').map(x => new Date(x)); //Arr of Str to Arr of Date objects
     let weekendDays = getWeekendDaysInMonth(dateArr);
 
     return weekendDays;
 }
 
 function getWeekendDaysInMonth(dateArr) {
-
     let weekendDays = [];
 
-    for (let i = 0; i < dateArr.length; i++) {
+    for (let i = 0; i < dateArr.length; i++) { // for each months days check isWeekend
         let month = dateArr[i].getMonth();
 
         while (dateArr[i].getMonth() === month) {
             if (isWeekendDay(dateArr[i])) {
-                weekendDays.push(dateArr[i].toISOString().slice(0, 10)); //коммент про moment()
+                weekendDays.push(dateArr[i].toISOString().slice(0, 10));
             }
 
             dateArr[i].setDate(dateArr[i].getDate() + 1);
@@ -59,5 +57,3 @@ function getWeekendDaysInMonth(dateArr) {
 
     return weekendDays;
 }
-
-//https://www.thepolyglotdeveloper.com/2017/08/unit-testing-node-js-application-jasmine-testing-framework/
